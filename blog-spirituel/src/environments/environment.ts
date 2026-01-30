72afe1342bc7e17004ba5af7d9419b09d2b45f30
Maryline Kouamé-Kodia <maryline.kouame-kodia@koralplay.com>
2026-01-30 11:07:44 +0100

    mieux

--- blog-spirituel/src/environments/environment.ts
+++ blog-spirituel/src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
     spaceId: 'tah5j0cyeq79',
     accessToken: 'xg6cWbrO6LJT3seWOIVOb1IBK3u9kIFwYvZXJDO7Y-c' // Clique sur "Show" dans Contentful pour le voir
   },
-  brevoApiKey: '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'
+  brevoApiKey: 'SECRET_KEY_ON_NETLIFY'
 };
